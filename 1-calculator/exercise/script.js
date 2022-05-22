67ef8d4d7a0c0ead9b3350711a5d3bc458200887
NazgulM <92852651+NazgulM@users.noreply.github.com>
2022-05-22 14:45:46 +0200

    our solutions

--- 1-calculator/exercise/script.js
+++ 1-calculator/exercise/script.js
@@ -1,6 +1,7 @@
 function multiply() {
   const number1 = parseFloat(document.querySelector("#multOne").value);
   const number2 = parseFloat(document.querySelector("#multTwo").value);
+  console.log(number1*number2)
 
   /* SOLUTION */
 }
@@ -9,6 +10,15 @@ function operation() {
   const number1 = parseFloat(document.querySelector("#opOne").value);
   const number2 = parseFloat(document.querySelector("#opTwo").value);
   const operation = document.querySelector("#operator").value;
+  if (operation === "addition") {
+    console.log(number1 + number2)
+  } else if (operation === "subtraction") {
+    console.log(number1 - number2)
+  } else if (operation === "division") {
+    console.log(number1 / number2)
+  } else if (operation === "multiplication") {
+    console.log(number1 * number2)
+  }
 
   /* SOLUTION */
 }
@@ -16,6 +26,11 @@ function operation() {
 function checkSame() {
   const number1 = parseFloat(document.querySelector("#checkOne").value);
   const number2 = parseFloat(document.querySelector("#checkTwo").value);
+  if (number1 === number2) {
+    console.log(number1 *number2)
+  } else {
+    console.log(number1 / number2)
+  }
 
   /* SOLUTION */
 }
@@ -24,6 +39,11 @@ function inRange() {
   const min = parseFloat(document.querySelector("#rangeMin").value);
   const max = parseFloat(document.querySelector("#rangeMax").value);
   const input = parseFloat(document.querySelector("#rangeInput").value);
+  if (input <= max && input >= min) {
+    console.log("Yes")
+  } else {
+    console.log("No")
+  }
 
   /* SOLUTION */
 }
--- 2-strings/exercise/script.js
+++ 2-strings/exercise/script.js
@@ -1,11 +1,16 @@
 function checkJavascript() {
   const input = document.querySelector("#jsTest").value;
-
+  if (input.includes("javascript")) {
+    console.log("Yes")
+  } else {
+    console.log("No")
+  }
   /* SOLUTION */
 }
 
 function vowels() {
   const input = document.querySelector("#vowelsInput").value;
+    console.log(input.match(/[aeiou]/gi).length)
 
   /* SOLUTION #1 */
 
@@ -13,8 +18,24 @@ function vowels() {
 }
 
 function isPalindrome() {
-  const input = document.querySelector("#palindrome").value;
+  let input = document.querySelector("#palindrome").value;
+  input = input.toLowerCase().replace(/[\W_]/g, '')
+
+  let reversedInput  = '';
+  for (let i of input) {
+    reversedInput =  i + reversedInput
+  }
+  let paragraph = document.querySelector('#palindromeResult');
+  
+  if (input === reversedInput){
+    console.log("Yes, it is a palindrome")
+    paragraph.innerHTML = 'Yes, it is a palindrome';
+  } else {
+    console.log("No, it is not a palindrome")
+  }
+  
+  
 
   /* SOLUTION #1 */
-  // Bonus point if you do a .lowercase() first
+  //  Bonus point if you do a .lowercase() first
 }
